a6e2ce340f3ba4bdc22d8329c9dd17fbf2e848a1
Fernando Calisto <fernandocalisto27@gmail.com>
2025-03-18 16:01:10 -0300

    feat(Lista de sorteados): Sortear Nomes.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,45 @@
 let listaNomes = document.getElementById('lista-amigos');
+let listaSorteados = document.getElementById('lista-sorteio');
+let participantes = [];
 
 function adicionar (){
     let nome = document.getElementById('nome-amigo').value;
-    listaNomes.innerHTML = `${listaNomes.innerHTML}${nome}<br>`;
+    listaNomes.innerHTML += `${nome}<br>`;
+    participantes.push(nome);
+}
+
+function embaralhar(array) {
+    let indiceAtual = array.length, indiceAleatorio;
+
+    while (indiceAtual > 0) {
+        indiceAleatorio = Math.floor(Math.random() * indiceAtual);
+        indiceAtual--;
+
+        [array[indiceAtual], array[indiceAleatorio]] = [
+            array[indiceAleatorio], array[indiceAtual]];
+    }
+
+    return array;
+}
+
+function sortear(){
+    if (participantes.length < 2) {
+        alert("Adicione pelo menos dois participantes!");
+        return;
+    }
+
+    let sorteados = [...participantes];
+    embaralhar(sorteados);
+
+    for(let i = 0; i < participantes.length; i++){
+        if(participantes[i] === sorteados[i]){
+            sortear();
+            return;
+        }
+    }
+
+    listaSorteados.innerHTML = '';
+    for(let i = 0; i < participantes.length; i++){
+        listaSorteados.innerHTML += `${participantes[i]} --> ${sorteados[i]}<br>`;
+    }
 }
